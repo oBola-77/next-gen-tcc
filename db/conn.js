e57b442b12a44e4af5a2e64b03bd7b2c459b4f9f
Pedro Abreu <opedroabreu21@gmail.com>
2024-10-11 01:03:05 -0300

    tentativa 13

--- db/conn.js
+++ db/conn.js
@@ -14,8 +14,8 @@ const requestHandler = async (req, res) => {
   const result = await sql`SELECT version()`;
   const { version } = result[0];
   res.render('index.html')
-  // res.writeHead(200, { "Content-Type": "text/plain" });
-  // res.end(`${version} é aqui`);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`${version} é aqui`);
 };
 
 http.createServer(requestHandler).listen(3000, () => {
