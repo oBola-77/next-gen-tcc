022c0d09141ac4f8d77a3c06a62869321400868a
Pedro Abreu <opedroabreu21@gmail.com>
2024-11-26 11:07:43 -0300

    AAAAAAAAAAAA

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import jwt from 'jsonwebtoken';
 import { verificarToken } from './middlewares/authMiddleware.js'
 
 const __filename = fileURLToPath(import.meta.url);
