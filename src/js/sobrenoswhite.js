eceaab105cbce036ef879ec6250a5e1f3cbcd80b
NathanHerrero <marquesnathan22@gmail.com>
2024-11-29 18:06:20 -0300

    .

--- src/js/sobrenoswhite.js
+++ src/js/sobrenoswhite.js
@@ -14,10 +14,10 @@ const inicio = document.querySelector('.body.light-mode .inicio');
 if (localStorage.getItem('light-mode') === 'enabled') {
     body.classList.add('light-mode');
     toggleCheckboxes.forEach((checkbox) => {
-        checkbox.checked = true; // Marca todos os checkboxes como ativados
+        checkbox.checked = true; // Marca os checkboxes como ativados, mas sem animação
     });
-  
-    // Ajusta elementos específicos para o light mode
+
+    // Ajusta elementos específicos para o light mode, sem animação
     if (customShapeDivider) {
         customShapeDivider.style.backgroundColor = '#ffffff';
         svgElement.style.fill = '#eaeaea';
@@ -27,13 +27,13 @@ if (localStorage.getItem('light-mode') === 'enabled') {
     }
 }
 
-// Alterna entre modo escuro e claro
+// Alterna entre modo escuro e claro ao clicar (com animação)
 toggleCheckboxes.forEach((toggleCheckbox) => {
     toggleCheckbox.addEventListener('change', function () {
         if (this.checked) {
             body.classList.add('light-mode');
 
-            // Ajusta elementos para o light mode
+            // Ajusta elementos para o light mode com animação
             if (customShapeDivider) {
                 customShapeDivider.style.backgroundColor = '#ffffff';
                 svgElement.style.fill = '#eaeaea';
@@ -46,7 +46,7 @@ toggleCheckboxes.forEach((toggleCheckbox) => {
         } else {
             body.classList.remove('light-mode');
 
-            // Reverte os ajustes para o dark mode
+            // Reverte os ajustes para o dark mode com animação
             if (customShapeDivider) {
                 customShapeDivider.style.backgroundColor = '#121212';
                 svgElement.style.fill = ''; // Volta ao estilo padrão
