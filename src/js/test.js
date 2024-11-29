be17899145d0fb581285e0c0ddab1164967113a9
Pedro Abreu <opedroabreu21@gmail.com>
2024-11-29 15:18:55 -0300

    finalizando pagina de usuario 2

--- src/js/test.js
+++ src/js/test.js
@@ -1,25 +1,13 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const user = await fetchTest();
-    console.log(user);
-    if (user) {
-        console.log("Puxando projetos");
-        const projetos = await fetchProjetos();
-        if (projetos) {
-            console.log("renderizando projetos");
-            renderizarProjetos(projetos);
-        }
-    }
-});
 
 async function fetchTest() {
     const token = localStorage.getItem('authToken');
-
+    
     if (!token) {
         console.log('Token não encontrado');
         window.location.href = 'login.html';
         return;
     }
-
+    
     await fetch('/test', {
         method: 'GET',
         headers: {
@@ -27,14 +15,15 @@ async function fetchTest() {
             'Content-Type': 'application/json'
         }
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Acesso não autorizado');
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Acesso não autorizado');
             }
             return response.json();
         })
         .then(data => {
             console.log('Resposta:', data);
+            return data
         })
         .catch(error => {
             console.error('Erro:', error);
@@ -44,44 +33,44 @@ async function fetchTest() {
 function renderizarProjetos(projetos) {
     console.log("Renderizando projetos");
     const container = document.getElementById('projetos-container');
-
+    
     container.innerHTML = '';
-
+    
     projetos.forEach(projeto => {
-
+        
         const card = document.createElement('div');
         card.classList.add('project-card');
-
+        
         card.innerHTML = `
-            <div class="project-image">
-                <img src="${projeto.imagem}" alt="Imagem do projeto">
-            </div>
-            <div class="project-info">
-                <span class="project-title">${projeto.nome}</span>
-                <p class="project-description">${projeto.descricao}</p>
+        <div class="project-image">
+        <img src="${projeto.imagem}" alt="Imagem do projeto">
+        </div>
+        <div class="project-info">
+        <span class="project-title">${projeto.nome}</span>
+        <p class="project-description">${projeto.descricao}</p>
                 <p class="project-description">Consultor: ${projeto.consultor}</p>
                 <p class="project-description">Status: ${projeto.status}</p>
                 <p class="project-description">Data de Conclusão: ${projeto.dataConclusao}</p>
-            </div>
-        `;
-
-        container.appendChild(card);
-    });
-}
-
-async function fetchProjetos() {
-    const token = localStorage.getItem('authToken');
-
-    if (!token) {
-        console.error('Token não encontrado. Faça login.');
-        return;
-    }
-
-    try {
-        const response = await fetch('/test');  
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
+                </div>
+                `;
+                
+                container.appendChild(card);
+            });
+        }
+        
+        async function fetchProjetos() {
+            const token = localStorage.getItem('authToken');
+            
+            if (!token) {
+                console.error('Token não encontrado. Faça login.');
+                return;
+            }
+            
+            try {
+                const response = await fetch('/test');  
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log("responde data:", data);
             return data.projetos;  
         } else {
             console.error("Erro ao buscar projetos", response.statusText);
@@ -90,3 +79,17 @@ async function fetchProjetos() {
         console.error("Erro ao buscar projetos:", error);
     }
 }
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const data = await fetchTest();
+    console.log(data);
+    console.log(data.user)
+    if (data) {
+        console.log("Puxando projetos");
+        const projetos = await fetchProjetos();
+        if (projetos) {
+            console.log("renderizando projetos");
+            renderizarProjetos(projetos);
+        }
+    }
+});
